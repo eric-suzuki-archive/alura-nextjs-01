0504428030eee3d7c8769e2bfc8f7e8a7872501c
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-24 00:23:48 -0300

    Module 01-Aula 06

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQ(){
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <a href="/">Ir para a home</a>
+        </div>
+    )
+}
